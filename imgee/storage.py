ac74212b5ff5ab94e1b1495ce749ffc1430f2159
Nigel Babu <nigel@hasgeek.com>
2012-08-22 14:53:44 +0530

    Upload files to S3

--- imgee/storage.py
+++ imgee/storage.py
@@ -9,11 +9,12 @@ def configure():
     """
     return connect_s3(app.config['AWS_ACCESS_KEY'], app.config['AWS_SECRET_KEY'])
 
-def upload(name, title, connection):
+def upload(name, title):
     """
     Upload a file to S3
     """
-    bucket = connection.create_bucket(app.config['AWS_BUCKET'])
+    conn = configure()
+    bucket = conn.create_bucket(app.config['AWS_BUCKET'])
     k = Key(bucket)
     k.key = name
     k.set_contents_from_filename(path.join(app.config['UPLOADED_FILES_DEST'], title))
--- imgee/views/index.py
+++ imgee/views/index.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
-
-from flask import render_template, request, flash
+from uuid import uuid4
+from flask import render_template, request, flash, g
 from imgee import app, uploadedfiles
 from imgee.forms import UploadForm
+from imgee.models import UploadedFile, db
+from imgee.views.login import lastuser
+from imgee.storage import upload
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/upload', methods=('GET', 'POST'))
+@lastuser.requires_login
 def upload_images():
     form = UploadForm(request.form)
     if form.validate_on_submit():
         filename = uploadedfiles.save(request.files['uploaded_file'])
+        uploaded_file = UploadedFile(name=uuid4().hex, title=filename, user=g.user)
+        db.session.add(uploaded_file)
+        db.session.commit()
+        upload(uploaded_file.name, uploaded_file.title)
         flash("File saved.")
+    elif request.method == 'POST':
+        flash("There was an error")
     return render_template('form.html', form=form)
 
