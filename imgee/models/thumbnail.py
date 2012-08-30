4f8ce51c2f610037f48e7da1e1476a436fe630db
Nigel Babu <nigel@hasgeek.com>
2012-08-30 22:59:07 +0530

    Add index to thumbnail name

--- imgee/models/thumbnail.py
+++ imgee/models/thumbnail.py
@@ -5,5 +5,6 @@ from imgee.models import db
 class Thumbnail(BaseMixin, db.Model):
     __tablename__ = 'thumbnail'
 
-    name = db.Column(db.Unicode(250), nullable=False, unique=True)
+    name = db.Column(db.Unicode(250), nullable=False, unique=True, index=True)
+    size = db.Column(db.Unicode(100), nullable=False, index=True)
     uploaded_file_id = db.Column(db.Integer, db.ForeignKey('file.id'))
