888cb413800eaaa6c74e34ba99d5afb0614cfee5
Nigel Babu <nigel@hasgeek.com>
2012-09-06 19:44:46 +0530

    Add Profile model

--- imgee/models/file_storage.py
+++ imgee/models/file_storage.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from coaster.sqlalchemy import BaseNameMixin
-from imgee.models import db
+from imgee.models import db, Profile
 
 
 class FileStorage(BaseNameMixin, db.Model):
     __tablename__ = 'file_storage'
 
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+    profile = db.relationship('Profile', backref='file_storage')
     thumbnails = db.relationship('Thumbnail', backref='file_storage')
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
