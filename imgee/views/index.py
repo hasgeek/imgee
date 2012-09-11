653f970e51da4bae8379fa41a1ee043ed0ee87c5
Nigel Babu <nigel@hasgeek.com>
2012-09-11 20:52:21 +0530

    Updated thumbnail view. It's not a resource endpoint

--- imgee/views/index.py
+++ imgee/views/index.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os.path
 from uuid import uuid4
-from flask import render_template, request, flash, g, jsonify, make_response
+from flask import render_template, request, flash, g, jsonify, make_response, redirect
 from imgee import app, uploadedfiles
 from imgee.forms import UploadForm
 from imgee.models import StoredFile, Thumbnail, db, Profile
@@ -45,22 +45,17 @@ def list_files(callerinfo):
 
 
 @app.route('/file/<filename>')
-@lastuser.resource_handler('imgee/thumbnail')
 def get_thumbnail(filename):
     make_profiles()
     size = request.args.get('size')
-    if not size:
-        return jsonify({'error': 'Size not specified'})
-    uploadedfile = StoredFile.query.filter_by(name=filename).first()
-    if not is_image(uploadedfile.name):
-        return jsonify({'error': 'File is not an image'})
+    stored_file = StoredFile.query.filter_by(name=filename).first()
+    converted_size = convert_size(size)
+    if not size or is_image(uploadedfile.name) or not converted_size:
+        return redirect('%s/%s' % (app.config['MEDIA_DOMAIN'], stored_file.name))
     existing_thumnail = Thumbnail.query.filter_by(size=size, uploadedfile=uploadedfile).first()
     if existing_thumbnail:
-        return jsonify({'url': '%s/%s' % (app.config['MEDIA_DOMAIN'], existing_thumnail.name)})
-    converted_size = convert_size(size)
-    if not converted_size:
-        return jsonify({'error': 'The size is invalid'})
+        return redirect('%s/%s' % (app.config['MEDIA_DOMAIN'], existing_thumnail.name))
     new_thumbnail = create_thumbnail(uploadedfile, converted_size)
     if new_thumbnail:
-        return jsonify({'url': '%s/%s' % (app.config['MEDIA_DOMAIN'], new_thumnail.name)})
-    return jsonify({'error': 'Thumbnail creation error'})
+        return redirect('%s/%s' % (app.config['MEDIA_DOMAIN'], new_thumnail.name))
+    return redirect('%s/%s' % (app.config['MEDIA_DOMAIN'], stored_file.name))
