a9572dd85a3ea78e7bcb3220896df2cb05c1aa34
Devi <asldevi@gmail.com>
2013-04-12 15:29:35 +0530

    fix profile-stored_file relationship

--- imgee/models/stored_file.py
+++ imgee/models/stored_file.py
@@ -18,7 +18,6 @@ class StoredFile(BaseNameMixin, db.Model):
     __tablename__ = 'stored_file'
 
     profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
-    profile = db.relationship('Profile', backref='stored_file')
     thumbnails = db.relationship('Thumbnail', backref='stored_file',
                                  cascade='all, delete-orphan')
     labels = db.relationship('Label', secondary='image_labels',
