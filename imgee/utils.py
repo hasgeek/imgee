4cfb615f26871446e157e6b55f8585776c0e0195
Devi <asldevi@gmail.com>
2013-05-01 14:51:40 +0530

    same interface to add/remove labels

--- imgee/utils.py
+++ imgee/utils.py
@@ -13,18 +13,21 @@ def delete_label(label):
     db.session.delete(label)
     db.session.commit()
 
-def save_labels_to(stored_file, labels):
-    new_labels = [l for l in labels if not l in stored_file.labels]
-    if new_labels:
-        stored_file.labels.extend(new_labels)
-        db.session.commit()
-    return new_labels
+def get_label_changes(nlabels, olabels):
+    if (nlabels == olabels):
+        status, diff = '0', []
+    elif (nlabels > olabels):
+        status, diff = '+', nlabels-olabels
+    elif (olabels > nlabels):
+        status, diff = '-', olabels-nlabels
+    else:
+        status, diff = '', nlabels
+    return status, list(diff)
 
-def remove_labels_from(stored_file, labels):
-    # improve this
-    to_remove = [l for l in labels if l in stored_file.labels]
-    for label in to_remove:
-        stored_file.labels.remove(label)
-    if to_remove:
+def save_labels_to(stored_file, labels):
+    new_labels = set(labels)
+    old_lables = set(stored_file.labels)
+    if new_labels != old_lables:
+        stored_file.labels = labels
         db.session.commit()
-    return to_remove
+    return get_label_changes(new_labels, old_lables)
