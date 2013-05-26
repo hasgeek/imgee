1f87f0fd103af36a9ae3f5bc7a6607d69be715d2
Devi <asldevi@gmail.com>
2013-05-26 16:52:38 +0530

    take redis config from production for rqworkers

--- rqconfig.py
+++ rqconfig.py
@@ -1,9 +1,11 @@
 # settings for the RQ worker
 # usage: rqworker -c rqconfig
 
+
 from urlparse import urlparse
 from imgee import init_for, app
 
+init_for('production')
 REDIS_URL = app.config.get('REDIS_URL', 'redis://localhost:6379/0')
 
 r = urlparse(REDIS_URL)
