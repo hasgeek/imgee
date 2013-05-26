defd547b15a67bf89524c19ee3fd53869b907a71
Devi <asldevi@gmail.com>
2013-05-26 16:44:26 +0530

    config for rq workers

---
+++ rqconfig.py
@@ -0,0 +1,16 @@
+# settings for the RQ worker
+# usage: rqworker -c rqconfig
+
+from urlparse import urlparse
+from imgee import init_for, app
+
+REDIS_URL = app.config.get('REDIS_URL', 'redis://localhost:6379/0')
+
+r = urlparse(REDIS_URL)
+REDIS_HOST = r.hostname
+REDIS_PORT = r.port
+REDIS_PASSWORD = r.password
+REDIS_DB = 0
+
+QUEUES = ['high', 'default']
+
