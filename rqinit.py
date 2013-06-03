03e95293dec863ca8818d8764c90546db04c9a61
Devi <asldevi@gmail.com>
2013-06-03 21:50:07 +0530

    parse redis URL and give to rqworkers

--- rqinit.py
+++ rqinit.py
@@ -1,3 +1,13 @@
 from imgee import init_for
 init_for('production')
 REDIS_URL = app.config['REDIS_URL']
+
+# REDIS_URL is not taken by setup_default_arguments function of rq/scripts/__init__.py
+# so, parse in pieces and give it
+
+r = urlparse(REDIS_URL)
+REDIS_HOST = r.hostname
+REDIS_PORT = r.port
+REDIS_PASSWORD = r.password
+REDIS_DB = 0
+
