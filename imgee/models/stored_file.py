94b0334860f1f0e8fcafbcf955b0d64a882fe155
Devi <asldevi@gmail.com>
2013-06-06 21:56:09 +0530

    add size, width, height on image pages

--- imgee/models/stored_file.py
+++ imgee/models/stored_file.py
@@ -34,6 +34,9 @@ class StoredFile(BaseNameMixin, db.Model):
     """
     __tablename__ = 'stored_file'
     profile_id = db.Column(None, db.ForeignKey('profile.id'), nullable=False)
+    size = db.Column(db.BigInteger, default=0, nullable=False)  # in bytes
+    width = db.Column(db.Integer, default=0)
+    height = db.Column(db.Integer, default=0)
     thumbnails = db.relationship('Thumbnail', backref='stored_file',
                                  cascade='all, delete-orphan')
     labels = db.relationship('Label', secondary='image_labels',
