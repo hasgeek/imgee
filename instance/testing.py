10f4a3acd19534c051197d3045c4384af9b09ede
Praseetha-KR <praseetha@hasgeek.com>
2013-06-21 01:19:26 +0530

    fixes

--- instance/testing.py
+++ instance/testing.py
@@ -4,15 +4,15 @@ AWS_FOLDER = 'test/'
 
 UNKNOWN_FILE_THUMBNAIL = 'unknown.jpeg'
 
-#: S3 Configuration
-AWS_ACCESS_KEY = 'AKIAINHIM7Q7GGWGDTVA'
-AWS_SECRET_KEY = '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'
-AWS_BUCKET = 'media.hasgeek.com'
-AWS_FOLDER = 'test/'     # set this if you want all files to be uploaded to a particular folder eg., 'test/'
-#: Domain name for files
-MEDIA_DOMAIN = 'http://media.hasgeek.com/'
+#: S3 Configuration; AWS credentials with restricted access
+AWS_ACCESS_KEY = environ.get('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = environ.get('AWS_SECRET_KEY')
+AWS_BUCKET = 'imgee-testing'
+AWS_FOLDER = 'test'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite://test.db'
+MEDIA_DOMAIN = 'https://%s.s3.amazonaws.com' % AWS_BUCKET
+
+SQLALCHEMY_DATABASE_URI = 'sqlite://'
 CSRF_ENABLED = False
 
 # redis settings for RQ
