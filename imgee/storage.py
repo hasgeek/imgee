6de73b07c77cfefb356a4334b859516eef0c5eec
Devi <asldevi@gmail.com>
2013-09-26 18:53:50 +0530

    put in async and move things to utils

--- imgee/storage.py
+++ imgee/storage.py
@@ -6,29 +6,12 @@ import mimetypes
 from StringIO import StringIO
 from PIL import Image
 
-from boto import connect_s3
-from boto.s3.bucket import Bucket
-from boto.s3.key import Key
-
 from imgee import app
-from imgee.models import db, Thumbnail
-from imgee.utils import newid, guess_extension
-from imgee.models import StoredFile
-
-
-def get_s3_bucket():
-    conn = connect_s3(app.config['AWS_ACCESS_KEY'], app.config['AWS_SECRET_KEY'])
-    bucket = Bucket(conn, app.config['AWS_BUCKET'])
-    return bucket
-
-
-def get_width_height(img_path):
-    try:
-        img = Image.open(img_path)
-    except:
-        return (0, 0)
-    else:
-        return img.size
+from imgee.models import db, Thumbnail, StoredFile
+from imgee.async import queueit
+from imgee.utils import (newid, guess_extension, get_file_type,
+                        path_for, get_s3_folder, get_s3_bucket,
+                        download_frm_s3, get_width_height)
 
 
 def save_in_db(name, title, local_path, profile, mimetype):
@@ -40,37 +23,25 @@ def save_in_db(name, title, local_path, profile, mimetype):
     db.session.commit()
 
 
-def save(fp, profile, img_name=None, remote=True):
+def save(fp, profile, img_name=None):
     id_ = img_name or newid()
     content_type = get_file_type(fp.filename)
     img_name = "%s%s" % (id_, guess_extension(content_type))
     local_path = path_for(img_name)
+
     with open(local_path, 'w') as img:
         img.write(fp.read())
 
     save_in_db(name=id_, title=fp.filename, local_path=local_path,
                     profile=profile, mimetype=content_type)
-
-    if remote:
-        save_on_s3(local_path, img_name, content_type=content_type)
+    queueit('save_on_s3', local_path, img_name, content_type=content_type)
 
 
-def get_file_type(filename):
-    return mimetypes.guess_type(filename)[0]
-
-
-def get_s3_folder(f=''):
-    f = f or app.config.get('AWS_FOLDER', '')
-    if f and not f.endswith('/'):
-        f = f + '/'
-    return f or ''
-
-
-def save_on_s3(file_path, filename='', content_type='', bucket='', folder=''):
+def save_on_s3(file_path, remotename='', content_type='', bucket='', folder=''):
+    b = bucket or get_s3_bucket()
+    folder = get_s3_folder(folder)
     with open(file_path) as fp:
-        b = bucket or get_s3_bucket()
-        folder = get_s3_folder(folder)
-        filename = filename or fp.name
+        filename = remotename or fp.name
         k = b.new_key(folder+filename)
         content_type = content_type or get_file_type(filename)
         headers = {
@@ -81,14 +52,16 @@ def save_on_s3(file_path, filename='', content_type='', bucket='', folder=''):
     return k
 
 
-def path_for(img_name):
-    return os.path.join(app.config['UPLOADED_FILES_DEST'], img_name)
-
-
 def parse_size(size):
     """return `size` as a tuple (w, h). None if not formattable"""
     if isinstance(size, (str, unicode)):
-        return split_size(size)
+        # return (w, h) if size is 'wxh'
+        r = r'^(\d+)(x(\d+))?$'
+        matched = re.match(r, size)
+        if matched:
+            w, h = matched.group(1, 2)
+            h = int(h.lstrip('x')) if h is not None else 0
+            return int(w), h
     elif isinstance(size, tuple) and len(size) == 2:
         return size
 
@@ -106,16 +79,6 @@ def get_resized_image(img, size, is_thumbnail=False):
     return img_name
 
 
-def get_image_locally(img_name):
-    local_path = path_for(img_name)
-    if not os.path.exists(local_path):
-        bucket = get_s3_bucket()
-        k = Key(bucket)
-        k.key = img_name
-        k.get_contents_to_filename(local_path)
-    return local_path
-
-
 def get_resized_name(img, size):
     w, h = size
     if w and h:
@@ -130,13 +93,16 @@ def get_resized_name(img, size):
 
 
 def resize_and_save(img, size, is_thumbnail=False):
-    extn = img.extn
-    src_path = get_image_locally(img.name+extn)
+    src_path = download_frm_s3(img.name + img.extn)
     scaled_img_name = get_resized_name(img, size)
     content_type = img.mimetype
     format = guess_extension(content_type).lstrip('.')
     scaled_path = resize_img(src_path, size, format, is_thumbnail=is_thumbnail)
-    save_on_s3(scaled_path, scaled_img_name+extn, content_type)
+
+    # separate queue for thumbnails so that we can give high priority to save them on S3
+    queue = app.config.get('THUMBNAIL_QUEUE')
+    queueit('save_on_s3', scaled_path, scaled_img_name+img.extn, content_type=content_type, queue=queue)
+
     size_s = "%sx%s" % size
     scaled = Thumbnail(name=scaled_img_name, size=size_s, stored_file=img)
     db.session.add(scaled)
@@ -145,7 +111,8 @@ def resize_and_save(img, size, is_thumbnail=False):
 
 
 def get_size((orig_w, orig_h), (w, h)):
-    # fit the image to the box along the smaller side and preserve aspect ratio.
+    # return the size to fit the image to the box
+    # along the smaller side and preserve aspect ratio.
     # w or h being 0 means preserve aspect ratio with that height or width
 
     if (h == 0) or (w <= h):
@@ -181,17 +148,6 @@ def resize_img(src, size, format, is_thumbnail):
     resized_path = '%s_%sx%s%s' % (name, size[0], size[1], extn)
     resized.save(resized_path, format=format, quality=100)
     return resized_path
-
-
-def split_size(size):
-    """ return (w, h) if size is 'wxh'
-    """
-    r = r'^(\d+)(x(\d+))?$'
-    matched = re.match(r, size)
-    if matched:
-        w, h = matched.group(1, 2)
-        h = int(h.lstrip('x')) if h is not None else 0
-        return int(w), h
 
 
 def delete_on_s3(stored_file):
--- imgee/utils.py
+++ imgee/utils.py
@@ -1,18 +1,74 @@
 # -*- coding: utf-8 -*-
 
+import os.path
 from uuid import uuid4
-from flask import request
-from imgee import app
 import mimetypes
+from PIL import Image
+
+from boto import connect_s3
+from boto.s3.bucket import Bucket
+from boto.s3.key import Key
+
+from flask import request
+
+from imgee import app
+
 
 def newid():
     return unicode(uuid4().hex)
+
 
 def get_media_domain():
     scheme = request.scheme
     return '%s:%s' % (scheme, app.config.get('MEDIA_DOMAIN'))
 
+def path_for(img_name):
+    return os.path.join(app.config['UPLOADED_FILES_DEST'], img_name)
+
+
+# -- mimetypes and content types
+
 def guess_extension(mimetype):
     if mimetype in ('image/jpg', 'image/jpe', 'image/jpeg'):
         return '.jpeg'    # guess_extension returns .jpe, which PIL doesn't like
     return mimetypes.guess_extension(mimetype)
+
+
+def get_file_type(filename):
+    return mimetypes.guess_type(filename)[0]
+
+# -- s3 related --
+
+def get_s3_bucket():
+    conn = connect_s3(app.config['AWS_ACCESS_KEY'], app.config['AWS_SECRET_KEY'])
+    bucket = Bucket(conn, app.config['AWS_BUCKET'])
+    return bucket
+
+
+def get_s3_folder(f=''):
+    f = f or app.config.get('AWS_FOLDER', '')
+    if f and not f.endswith('/'):
+        f = f + '/'
+    return f or ''
+
+
+def download_frm_s3(img_name):
+    local_path = path_for(img_name)
+    if not os.path.exists(local_path):
+        bucket = get_s3_bucket()
+        k = Key(bucket)
+        k.key = img_name
+        k.get_contents_to_filename(local_path)
+    return local_path
+
+
+# -- image details --
+
+def get_width_height(img_path):
+    try:
+        img = Image.open(img_path)
+    except IOError:
+        return (0, 0)
+    else:
+        return img.size
+
