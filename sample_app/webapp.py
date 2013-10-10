04b66f4cd52b11b545de673203b53971e86f8df7
Devi <asldevi@gmail.com>
2013-10-10 19:31:37 +0530

    use Imgee field from baseframe

--- sample_app/webapp.py
+++ sample_app/webapp.py
@@ -1,13 +1,32 @@
 from flask import render_template, Flask
+from baseframe.forms.fields import ImgeeField
+from flask.ext.wtf import Form
+from wtforms.validators import Required
+from baseframe import baseframe
 
 app = Flask(__name__)
 
-imgee_url = app.config.get('IMGEE_ENDPOINT')
+imgee_url = app.config.get('IMGEE_ENDPOINT', 'http://0.0.0.0:4500')
+
+app.config['SECRET_KEY'] = 'not set'
+
+class ImgeeForm(Form):
+    image = ImgeeField(label='Logo', description='Your company logo here',
+           validators=[Required()],
+           profile='asldevi', img_label='logos', img_size='100x75'
+    )
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', imgee_url=imgee_url)
+    form = ImgeeForm()
+    return render_template('index.html', form=form)
+
+
+def init_for(env):
+    baseframe.init_app(app, requires=['baseframe'])
 
 if __name__ == "__main__":
+    init_for('dev')
     app.run(port=4333, debug=True)
 
