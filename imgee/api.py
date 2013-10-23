f27e577864e214b02ac3dac6d64e34c34e3f1b7b
Devi <asldevi@gmail.com>
2013-10-23 12:34:10 +0530

    add upload endpoint to api

--- imgee/api.py
+++ imgee/api.py
@@ -1,23 +1,48 @@
 from flask import jsonify, request
 from coaster.views import load_model, load_models
 
-from imgee import app
-from imgee.models import db, StoredFile
-import async, utils
+from imgee import app, lastuser
+from imgee.models import db, StoredFile, Profile
+import async, utils, storage
 
 
-@app.route('/api/file/<image>')
+class Status(object):
+    ok = 'OK'
+    in_process = 'Processing'
+
+
+@app.route('/api/file/<image>.json')
 @load_model(StoredFile, {'name': 'image'}, 'image')
 def get_image_json(image):
     size = request.args.get('size')
     d = {}
     try:
         d['url'] = utils.get_image_url(image, size)
-        d['status'] = 'OK'
     except async.StillProcessingException as e:
         imgname = e.args[0]
         d['url'] = utils.get_url(imgname)
-        d['status'] = 'Processing'
+        d['status'] = Status.in_process
+    else:
+        d['status'] = Status.ok
+
     return jsonify(d)
 
+
+@app.route('/api/<profile>/new.json', methods=['POST'])
+@load_model(Profile, {'name': 'profile'}, 'profile'),
+    permission=['new-file', 'siteadmin']), addlperms=lastuser.permissions)
+def upload_file_json(profile):
+    file_ = request.files['file']
+    title, job = storage.save(file_, profile=profile)
+    try:
+        imgname = async.get_async_result(job)
+    except async.StillProcessingException as e:
+        imgname = e.args[0]
+        status = Status.in_process
+    else:
+        status = Status.ok
+
+    url = utils.get_url(imgname)
+    d = dict(url=url, status=status)
+    return jsonify(d)
 
