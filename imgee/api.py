23745cdc0b7929b9bc445af33f49e7102aed534d
Devi <asldevi@gmail.com>
2013-10-23 13:35:40 +0530

    make api a blueprint

--- imgee/api.py
+++ imgee/api.py
@@ -1,35 +1,35 @@
-from flask import jsonify, request
+from flask import jsonify, request, Blueprint
 from coaster.views import load_model, load_models
 
 from imgee import app, lastuser
 from imgee.models import db, StoredFile, Profile
 import async, utils, storage
 
+api = Blueprint('api', __name__)
 
 class Status(object):
     ok = 'OK'
     in_process = 'Processing'
 
 
-@app.route('/api/file/<image>.json')
+@api.route('/file/<image>.json')
 @load_model(StoredFile, {'name': 'image'}, 'image')
 def get_image_json(image):
     size = request.args.get('size')
-    d = {}
     try:
-        d['url'] = utils.get_image_url(image, size)
+        url = utils.get_image_url(image, size)
     except async.StillProcessingException as e:
         imgname = e.args[0]
-        d['url'] = utils.get_url(imgname)
-        d['status'] = Status.in_process
+        url = utils.get_url(imgname)
+        status = Status.in_process
     else:
-        d['status'] = Status.ok
-
+        status = Status.ok
+    d = dict(url=url, status=status)
     return jsonify(d)
 
 
-@app.route('/api/<profile>/new.json', methods=['POST'])
-@load_model(Profile, {'name': 'profile'}, 'profile'),
+@api.route('/<profile>/new.json', methods=['POST'])
+@load_model(Profile, {'name': 'profile'}, 'profile',
     permission=['new-file', 'siteadmin']), addlperms=lastuser.permissions)
 def upload_file_json(profile):
     file_ = request.files['file']
