8c7edbbb1915920715b323378d6b06b12976778b
Devi <asldevi@gmail.com>
2013-10-28 15:59:19 +0530

    put in 404 check in api

--- imgee/api.py
+++ imgee/api.py
@@ -1,4 +1,5 @@
 from flask import jsonify, request, Blueprint, url_for
+from werkzeug.exceptions import HTTPException
 from coaster.views import load_model, load_models
 import os
 from urlparse import urljoin
@@ -11,12 +12,18 @@ api = Blueprint('api', __name__)
 
 class Status(object):
     ok = 'OK'
-    in_process = 'Processing'
+    in_process = 'PROCESSING'
+    notfound = 'NOT FOUND'
 
 
-@api.route('/file/<image>.json')
-@load_model(StoredFile, {'name': 'image'}, 'image')
-def get_image_json(image):
+@api.route('/file/<imgname>.json')
+def get_image_json(imgname):
+    try:
+        image = StoredFile.query.filter_by(name=imgname).first_or_404()
+    except HTTPException:
+        return jsonify({"status": Status.notfound, "status_code": 404,
+                            "error": "Image with name '%s' doesn't exist." % imgname})
+
     size = request.args.get('size')
     try:
         url = utils.get_image_url(image, size)
@@ -26,15 +33,20 @@ def get_image_json(image):
         status = Status.in_process
     else:
         status = Status.ok
-
     imgee_url = urljoin(request.host_url, url_for('get_image', image=image.name, size=size))
-    d = dict(url=url, status=status, imgee_url=imgee_url)
+
+    d = dict(url=url, status=status, imgee_url=imgee_url, status_code=200)
     return jsonify(d)
 
 
-@api.route('/<profile>/new.json', methods=['POST'])
-@load_model(Profile, {'name': 'profile'}, 'profile')
-def upload_file_json(profile):
+@api.route('/<profilename>/new.json', methods=['POST'])
+def upload_file_json(profilename):
+    try:
+        profile = Profile.query.filter_by(name=profilename).first_or_404()
+    except HTTPException:
+        return jsonify({"status": Status.notfound, "status_code": 404,
+                        "error": "Profile with name '%s' doesn't exist." % profilename})
+
     file_ = request.files['file']
     title, job = storage.save(file_, profile=profile)
     try:
@@ -48,6 +60,6 @@ def upload_file_json(profile):
     url = utils.get_url(imgname)
     imgname = os.path.splitext(imgname)[0]
     imgee_url = urljoin(request.host_url, url_for('get_image', image=imgname))
-    d = dict(url=url, status=status, imgee_url=imgee_url)
+    d = dict(url=url, status=status, imgee_url=imgee_url, status_code=200)
     return jsonify(d)
 
