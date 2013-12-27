937c23e53fdb32f5f05b5ef6e9d6ab92f0807866
Mitesh Ashar <email@miteshashar.com>
2013-12-27 23:36:17 +0530

    StoredFile.orig_extn column

--- imgee/models/stored_file.py
+++ imgee/models/stored_file.py
@@ -39,6 +39,7 @@ class StoredFile(BaseNameMixin, db.Model):
     width = db.Column(db.Integer, default=0)
     height = db.Column(db.Integer, default=0)
     mimetype = db.Column(db.Unicode(32), nullable=False)
+    orig_extn = db.Column(db.Unicode(15), nullable=True)
     thumbnails = db.relationship('Thumbnail', backref='stored_file',
                                  cascade='all, delete-orphan')
     labels = db.relationship('Label', secondary='image_labels',
