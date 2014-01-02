576d556e83aad30bfbcfe94dc7bffc51997334d5
Mitesh Ashar <email@miteshashar.com>
2014-01-03 03:29:02 +0530

    Removed command for rsvg-convert, as ImageMagick is capable of processing svg's. In fact, it uses rsvg-convert as a delegate for svg's.

--- imgee/storage.py
+++ imgee/storage.py
@@ -221,29 +221,17 @@ def resize_img(src, dest, size, mimetype, format, is_thumbnail):
         return
     if not size:
         return src
-    
-    processed = False
-    
-    if 'processor' in ALLOWED_MIMETYPES[mimetype]:
-        if ALLOWED_MIMETYPES[mimetype]['processor'] == 'rsvg-convert':
-            try:
-                check_call('rsvg-convert --width=%s --height=%s --keep-aspect-ratio=TRUE --format=%s %s > %s'
-                    % (size[0], size[1], format, src, dest), shell=True)
-            except CalledProcessError as e:
-                return False
-            processed = True
-    if not processed:
-        try:
-            check_call('convert -resize %sx%s %s %s' % (size[0], size[1], src, dest), shell=True)
-        except CalledProcessError:
-            return False
+    try:
+        check_call('convert -resize %sx%s %s %s' % (size[0], size[1], src, dest), shell=True)
+    except CalledProcessError:
+        return False
 
-        # if is_thumbnail:
-        #     # and crop the rest, keeping the center.
-        #     w, h = resized.size
-        #     tw, th = map(int, app.config.get('THUMBNAIL_SIZE').split('x'))
-        #     left, top = int((w-tw)/2), int((h-th)/2)
-        #     resized = resized.crop((left, top, left+tw, top+th))
+    # if is_thumbnail:
+    #     # and crop the rest, keeping the center.
+    #     w, h = resized.size
+    #     tw, th = map(int, app.config.get('THUMBNAIL_SIZE').split('x'))
+    #     left, top = int((w-tw)/2), int((h-th)/2)
+    #     resized = resized.crop((left, top, left+tw, top+th))
 
     return True
 
