163fae89c9d672279f6dad9f4f9a2e37ebd3574b
Mitesh Ashar <email@miteshashar.com>
2014-01-05 23:40:08 +0530

    Add -dUseCIEColor to all calls to ghostscript(as ImageMagick's delegate), for better conversion of thumbnails of CMYK images. http://blog.digitaljohn.co.uk/2007/06/19/imagemagick-cmyk-to-rgb/

--- install_libraries.sh
+++ install_libraries.sh
@@ -3,13 +3,17 @@ if [ $(uname) == "Linux" ]
 then
     sudo apt-get install libjpeg-dev libpng-dev libfreetype6-dev liblcms1-dev libtiff4-dev ghostscript
     sudo apt-get install imagemagick
+    sudo sed -i.bak "s/\&quot;gs\&quot;/\&quot;gs\&quot; -dUseCIEColor/g" $(convert -list delegate | grep Path | sed -e "s/Path: //g")
+    sudo sed -r -i.bak "s/(-dUseCIEColor )+/-dUseCIEColor /g" $(convert -list delegate | grep Path | sed -e "s/Path: //g")
 elif [ $(uname) == "Darwin" ]
 then
     which brew > /dev/null
     if [ $? -eq 0 ]
     then
-        brew install libjpeg libpng freetype lcms2 libtiff ghostscript librsvg
+        brew install libjpeg libpng freetype lcms2 libtiff ghostscript librsvg gnu-sed
         brew install imagemagick --with-libtiff --enable-hdri --with-gslib
+        gsed -r -i.bak "s/\&quot;gs\&quot;/\&quot;gs\&quot; -dUseCIEColor/g" $(convert -list delegate | grep Path | sed -e "s/Path: //g")
+        gsed -r -i.bak "s/(-dUseCIEColor )+/-dUseCIEColor /g" $(convert -list delegate | grep Path | sed -e "s/Path: //g")
     else
         echo "Homebrew is not installed. Please install it. Check http://brew.sh for more."
         open http://brew.sh
