4ca7b883e920e129cd98d5f0903ac12fd73c4517
Mitesh Ashar <email@miteshashar.com>
2014-01-05 23:44:01 +0530

    Added librsvg2-dev to be installed for linux

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $(uname) == "Linux" ]
 then
-    sudo apt-get install libjpeg-dev libpng-dev libfreetype6-dev liblcms1-dev libtiff4-dev ghostscript
+    sudo apt-get install libjpeg-dev libpng-dev libfreetype6-dev liblcms1-dev libtiff4-dev librsvg2-dev ghostscript
     sudo apt-get install imagemagick
     sudo sed -i.bak "s/\&quot;gs\&quot;/\&quot;gs\&quot; -dUseCIEColor/g" $(convert -list delegate | grep Path | sed -e "s/Path: //g")
     sudo sed -r -i.bak "s/(-dUseCIEColor )+/-dUseCIEColor /g" $(convert -list delegate | grep Path | sed -e "s/Path: //g")
