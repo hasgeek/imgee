981656eeeb988d948eed923050fabb6ffe0e9150
Bibhas <iambibhas@gmail.com>
2017-05-04 12:59:58 +0530

    Make init_for call implicit. Fixes #63

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@
 from coaster.manage import init_manager
 
 from imgee.models import db
-from imgee import app, init_for
+from imgee import app
 
 
 if __name__ == "__main__":
     db.init_app(app)
-    manager = init_manager(app, db, init_for)
+    manager = init_manager(app, db)
     manager.run()
--- runcelery.py
+++ runcelery.py
@@ -1,14 +1,15 @@
 
-## Usage: $python runcelery.py `env` -A 'imgee.storage' worker -l info
-## `env` could be 'dev' for development or 'prod' for 'production'
+# Usage: $python runcelery.py `env` -A 'imgee.storage' worker -l info
+# `env` could be 'dev' for development or 'prod' for 'production'
 
 import sys
-from imgee import init_for, app, celery
+from imgee import app, celery
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print >> sys.stderr, "Usage: {} [env] [celery parameters]".format(sys.argv[0])
         sys.exit(1)
-    init_for(sys.argv[1])
+    # ignore env => sys.argv[1]
+    # on production app is already configured for env=prod
     with app.app_context():
         celery.start([sys.argv[0]] + sys.argv[2:])
