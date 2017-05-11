dfdf09af6ed5d3faaa5639133e99431b400e54a9
Bibhas <iambibhas@gmail.com>
2017-05-11 11:47:45 +0530

    reordered functions and better string formating

--- imgee/tasks.py
+++ imgee/tasks.py
@@ -13,35 +13,43 @@ class TaskRegistry(object):
         self.connection = redis.from_url(url)
         return self.connection
 
-    def _key(self, taskid):
-        return "{prefix}:{taskid}".format(prefix=self.key_prefix, taskid=taskid)
+    def _make_key(self, taskid):
+        return "{key_prefix}:{taskid}".format(key_prefix=self.key_prefix, taskid=taskid)
 
     def __contains__(self, taskid):
-        return len(self.connection.keys(self._key(taskid))) > 0
+        return len(self.connection.keys(self._make_key(taskid))) > 0
 
     def add(self, taskid):
-        self.connection.set(self._key(taskid), taskid)
+        self.connection.set(self._make_key(taskid), taskid)
         # setting TTL of 60 seconds for the key
         # if the file doesn't get processed within 60 seconds,
         # it'll be removed from the eregistry
-        self.connection.expire(self._key(taskid), 60)
-
-    def remove(self, taskid):
-        self.remove_by_key(self._key(taskid))
-
-    def remove_by_key(self, key):
-        self.connection.delete(key)
+        self.connection.expire(self._make_key(taskid), 60)
 
     def search(self, query):
-        return filter(lambda k: str(query) in k, self.connection.keys("*" + self._key(query + "*")))
+        # >> KEYS imgee:registry:default:*query*
+        return self.connection.keys(self._make_key("*{}*".format(query)))
 
     def get_all_keys(self):
         # >> KEYS imgee:registry:default:*
-        return list(self.connection.keys(self._key("*")))
+        return self.connection.keys(self._make_key("*"))
 
     def keys_starting_with(self, query):
         # >> KEYS imgee:registry:default:query*
-        return self.connection.keys(self._key(query + "*"))
+        return self.connection.keys(self._make_key("{}*".format(query)))
+
+    def remove(self, taskid):
+        # remove a key with the taskid
+        self.remove_by_key(self._make_key(taskid))
+
+    def remove_by_key(self, key):
+        # remove a redis key directly
+        self.connection.delete(key)
+
+    def remove_keys_matching(self, query):
+        keys = self.search(query)
+        for k in keys:
+            self.remove_by_key(k)
 
     def remove_keys_starting_with(self, query):
         keys = self.keys_starting_with(query)
