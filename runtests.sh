81ccc8d3beb36b1c10f50216d053e45c7bcb0a06
Bibhas <iambibhas@gmail.com>
2017-06-20 09:13:02 +0530

    trying to fix travis build

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,5 @@
 set -e
 export FLASK_ENV="TESTING"
 ./manage.py init  # creates the test upload directory
-coverage run `which nosetests` --with-timer "$@"
+coverage run `which nosetests --with-timer` "$@"
 coverage report -m
