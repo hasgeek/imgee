d398970bad51a81ea962ede739f169e13f5f1ce2
Bibhas <me@bibhas.in>
2019-08-19 14:53:59 +0530

    restored shell.py

--- shell.py
+++ shell.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
+import os  # NOQA # isort:skip
+import readline  # NOQA # isort:skip
+from pprint import pprint  # NOQA # isort:skip
+
+from flask import *  # NOQA # isort:skip
+from imgee import *  # NOQA # isort:skip
 
 os.environ['PYTHONINSPECT'] = 'True'
