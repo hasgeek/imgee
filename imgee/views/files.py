76070c4281819bb06f3e0efc0c29de83d9e9a933
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-07-21 01:49:13 +0530

    Set form nonce for async form

--- imgee/views/files.py
+++ imgee/views/files.py
@@ -39,6 +39,7 @@ def _redirect_url_frm_upload(profile_name):
 )
 def upload_file(profile):
     upload_form = UploadImageForm()
+    upload_form.form_nonce.data = upload_form.form_nonce.default()
     if upload_form.validate_on_submit():
         imgfile = request.files['upload_file']
         if imgfile.filename != '':
