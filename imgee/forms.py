09498c0204e0d757197ed8f7517181a7f989c793
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-06-07 02:56:27 +0530

    Upgrade to SQLAlchemy 1.4 and use forms via Baseframe

--- imgee/forms.py
+++ imgee/forms.py
@@ -1,7 +1,4 @@
-from wtforms import FileField, HiddenField, SelectField, TextField
-from wtforms.validators import Length, Required, ValidationError
-
-from baseframe.forms import Form
+from baseframe import forms
 from coaster.utils import make_name
 
 from . import app
@@ -11,20 +8,22 @@ from .utils import is_file_allowed
 
 def valid_file(form, field):
     if not is_file_allowed(field.data.stream, field.data.mimetype, field.data.filename):
-        raise ValidationError(
+        raise forms.ValidationError(
             "Sorry, unknown image format. Please try uploading another file."
         )
 
 
-class UploadImageForm(Form):
-    upload_file = FileField("File", validators=[Required(), valid_file])
+class UploadImageForm(forms.Form):
+    upload_file = forms.FileField(
+        "File", validators=[forms.validators.DataRequired(), valid_file]
+    )
 
 
-class DeleteImageForm(Form):
+class DeleteImageForm(forms.Form):
     pass
 
 
-class PurgeCacheForm(Form):
+class PurgeCacheForm(forms.Form):
     pass
 
 
@@ -42,46 +41,60 @@ def label_doesnt_exist(form, field):
     profile_id = form.profile_id.data
     label_name = make_name(field.data)
     if label_name in reserved_words():
-        raise ValidationError(
+        raise forms.ValidationError(
             "“%s” is reserved and cannot be used as a label. Please try another name."
             % label_name
         )
 
     exists = Label.query.filter_by(profile_id=profile_id, name=label_name).first()
     if exists:
-        raise ValidationError(
+        raise forms.ValidationError(
             "Label “%s” already exists. Please try another name." % field.data
         )
 
 
-class CreateLabelForm(Form):
-    label = TextField(
-        'Label', validators=[Required(), Length(max=250), label_doesnt_exist]
+class CreateLabelForm(forms.Form):
+    label = forms.StringField(
+        'Label',
+        validators=[
+            forms.validators.DataRequired(),
+            forms.validators.Length(max=250),
+            label_doesnt_exist,
+        ],
     )
-    profile_id = HiddenField('profile_id')
+    profile_id = forms.HiddenField('profile_id')
 
 
-class AddLabelForm(Form):
-    stored_file_id = HiddenField('stored_file_id')
-    labels = HiddenField('labels')
+class AddLabelForm(forms.Form):
+    stored_file_id = forms.HiddenField('stored_file_id')
+    labels = forms.HiddenField('labels')
 
 
-class RemoveLabelForm(Form):
+class RemoveLabelForm(forms.Form):
     pass
 
 
-class EditTitleForm(Form):
-    file_name = HiddenField('file_name')
-    file_title = TextField('title', validators=[Required(), Length(max=250)])
+class EditTitleForm(forms.Form):
+    file_name = forms.HiddenField('file_name')
+    file_title = forms.StringField(
+        'title',
+        validators=[forms.validators.DataRequired(), forms.validators.Length(max=250)],
+    )
 
 
-class UpdateTitle(Form):
-    title = TextField('Title', validators=[Required(), Length(max=250)])
+class UpdateTitle(forms.Form):
+    title = forms.StringField(
+        'Title',
+        validators=[forms.validators.DataRequired(), forms.validators.Length(max=250)],
+    )
 
 
-class EditLabelForm(Form):
-    label_name = TextField('label', validators=[Required(), Length(max=250)])
+class EditLabelForm(forms.Form):
+    label_name = forms.StringField(
+        'label',
+        validators=[forms.validators.DataRequired(), forms.validators.Length(max=250)],
+    )
 
 
-class ChangeProfileForm(Form):
-    profiles = SelectField('Profiles')
+class ChangeProfileForm(forms.Form):
+    profiles = forms.SelectField('Profiles')
